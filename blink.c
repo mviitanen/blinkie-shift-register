15e522ceba58c199c434746f37a6c9e80f781353
Marko Viitanen <marko.viitanen@familysearch.org>
2013-05-29 16:24:44 -0600

    Initial commit

---
+++ blink.c
@@ -0,0 +1,68 @@
+/* 
+ * File:   blink.c
+ * Author: viitanenm
+ *
+ * Created on April 5, 2013, 11:45 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <p24FJ64GB002.h>
+
+#define DELAY 4000
+
+#define DS_low()  LATB &=~0x8000
+#define DS_high()  LATB |=0x8000
+
+#define ST_CP_low() LATB &=~0x4000
+#define ST_CP_high() LATB |=0x4000
+
+#define SH_CP_low() LATB &=~0x2000
+#define SH_CP_high() LATB |=0x2000
+
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+
+void doStep(unsigned int pattern);
+void setLEDs(unsigned int pattern);
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    TRISB = 0x0000;
+    T1CON = 0x8030;
+
+    // reset everything
+    LATB = 0x0000;
+    doStep(0x00);
+    while (1) {
+        doStep(0x2000);
+        doStep(0x8000);
+        doStep(0x0080);
+    } // main loop
+    return (EXIT_SUCCESS);
+}
+
+void doStep(unsigned int pattern) {
+    TMR1 = 0;
+    setLEDs(pattern);
+    while (TMR1 < DELAY) {
+    }
+}
+
+void setLEDs(unsigned int pattern) {
+    ST_CP_low();
+    SH_CP_low();
+    int i;
+    for (i = 0; i < 16; i++) {
+        if (CHECK_BIT(pattern, i))
+            DS_high();
+        else
+            DS_low();
+
+        SH_CP_high();
+        SH_CP_low();
+    }
+    ST_CP_high();
+}
